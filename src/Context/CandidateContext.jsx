773b9dca5a5c7cb281cea4c710cadcaf5526bbbc
Pratik Zajam <44423338+pratikzajam@users.noreply.github.com>
2025-10-22 20:58:26 +0530

    added redme file

--- src/Context/CandidateContext.jsx
+++ src/Context/CandidateContext.jsx
@@ -129,10 +129,13 @@ export let CandidateProvider = ({ children }) => {
 
 
 
-
     useEffect(() => {
+        console.log("User from localStorage:", user);
         if (user) {
+            console.log("Fetching metrics...");
             getmetricsData();
+        } else {
+            console.log("No user found — skipping fetch");
         }
     }, [user]);
 
